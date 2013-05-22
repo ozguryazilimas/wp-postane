1955720efc5b14d72f24eaa80f163bdb8d4c1c3d
Onur Küçük <onur@delipenguen.net>
2013-05-23 00:25:03 +0300

    update basic-comment-quicktags

--- basic-comment-quicktags/uninstall.php
+++ basic-comment-quicktags/uninstall.php
@@ -5,4 +5,4 @@
 if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
 	exit();
     
-		register_setting('discussion','ippy_bcq_options');
+		unregister_setting('discussion','ippy_bcq_options');
