79eab3a8e0ab3c3c67031d5d01bc1a76bb91f2cb
Enes Gür <enes19972010@gmail.com>
2013-06-19 12:15:59 +0300

    refs #5102 daves-wordpress-live-search eklentisi kaldırıldı

--- daves-wordpress-live-search/test/test_callbacks.js
+++
@@ -1,4 +0,0 @@
-LiveSearch.addCallback('BeforeShowResults', function() { console.log('BeforeShowResults'); });
-LiveSearch.addCallback('AfterShowResults', function() { console.log('AfterShowResults'); });
-LiveSearch.addCallback('BeforeHideResults', function() { console.log('BeforeHideResults'); });
-LiveSearch.addCallback('AfterHideResults', function() { console.log('AfterHideResults'); });
