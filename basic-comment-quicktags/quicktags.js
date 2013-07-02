e07fbd653734423866714e2f74b055aae844f27e
Enes Gür <enes19972010@gmail.com>
2013-07-02 16:58:46 +0300

    refs #6600 Editör Düzenlemesi

--- basic-comment-quicktags/quicktags.js
+++ basic-comment-quicktags/quicktags.js
@@ -16,6 +16,14 @@
       id: "posttext",
       buttons: "link,em,strong"
     });
-    QTags.addButton('image','resim','[resim]','[/resim]','I','');
+   // QTags.addButton('image','resim','[resim]','[/resim]','I','');
+        QTags.addButton( 'tag', 'resim', prompt_image );
+    function prompt_image(e, c, ed) {
+        prmt = prompt('Resim adresini girin:');
+        if ( prmt === null ) return;
+        rtrn = '[resim]' + prmt + '[/resim]';
+        this.tagStart = rtrn;
+        QTags.TagButton.prototype.callback.call(this, e, c, ed);
+    }
 //   });
 // })(jQuery);
