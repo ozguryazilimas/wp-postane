4bba756b799b516f05f23744320ac45a76f2f353
Enes Gür <enes19972010@gmail.com>
2013-07-02 13:14:44 +0300

    refs #6600 yorumlara resim eklenebilmesi için lazım olmayan eklentiler kaldırıldı.

--- wordpress-comment-images/js/comment-images.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($){
-	$('#addCommentImage').click(function(){
-		var imageLoc = prompt('Enter the Image URL:');
-		if ( imageLoc ) {
-			$('#comment').val($('#comment').val() + '[img]' + imageLoc + '[/img]');
-		}
-		return false;
-	});
-});
