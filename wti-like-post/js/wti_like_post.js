4647a06c398d5a3e6a084e219de37f7de205811c
Onur Küçük <onur@delipenguen.net>
2014-01-25 01:39:03 +0200

    refs #9881 change digg star (tutma) when it is clicked

--- wti-like-post/js/wti_like_post.js
+++ wti-like-post/js/wti_like_post.js
@@ -2,7 +2,14 @@ jQuery(document).ready(function(){
      jQuery(".like img, .unlike img").click(function(){
           var task = jQuery(this).attr("rel");
           var post_id = jQuery(this).attr("id");
-          
+
+          var yildiz = 'dyildiz.png';
+          var current_yildiz = jQuery(this).attr('src');
+
+          if (current_yildiz.endsWith(yildiz)) {
+              yildiz = 'byildiz.png';
+          }
+
           if(task == "like")
           {
                post_id = post_id.replace("like-", "");
@@ -19,6 +26,8 @@ jQuery(document).ready(function(){
                url: blog_url + "/wp-content/plugins/wti-like-post/wti_like.php",
                data: "post_id=" + post_id + "&task=" + task + "&num=" + Math.random(),
                success: function(data){
+                    // this should be img#like- and img#unlike- but we are using this plugin wrong...
+                    jQuery('img#like-' + post_id).attr('src', blog_url + '/wp-content/plugins/wti-like-post/images/' + yildiz);
                     jQuery("#lc-" + post_id).html(data.like);
                     jQuery("#unlc-" + post_id).html(data.unlike);
                     jQuery("#status-" + post_id).removeClass("loading-img").empty().html(data.msg);
