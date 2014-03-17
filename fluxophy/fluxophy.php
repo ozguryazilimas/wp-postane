7d0f314fcb2f85dc8c3984e9228b3847e2a4bf7e
Onur Küçük <onur@delipenguen.net>
2014-03-17 16:10:59 +0200

    refs #10278 initial commit for fluxophy plugin

---
+++ fluxophy/fluxophy.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Plugin Name: Fluxophy
+Plugin URI: http://www.ozguryazilim.com.tr
+Description: This plugin fetches external JSON sources and displays them in a widget. For example, you can show facebook feeds with URL http://www.facebook.com/feeds/page.php?format=json&id=YOUR_ID
+Version: 0.5.0
+Author: Onur Küçük
+Author URI: http://www.delipenguen.net
+License: GPL2
+*/
+
+/*  Copyright (C) 2014, Onur Küçük
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+    define('FLUXOPHY_DISPLAY_COUNT', 10);
+    require_once(dirname(__FILE__) . '/includes/class-wp-fp-widget.php');
+    $wp_fp = new WP_Fluxophy_Widget();
+
+    global $wpdb;
+
+    add_action('widgets_init', 'fluxophy_init');
+    add_action('init', 'fluxophy_add_css');
+
+
+    function fluxophy_add_css() {
+        wp_enqueue_style('fluxophy', get_option('siteurl') . '/wp-content/plugins/fluxophy/css/fluxophy.css');
+    }
+
+    function fluxophy_init() {
+        load_plugin_textdomain('fluxophy', false, basename(dirname(__FILE__)) . '/languages' );
+        register_widget('WP_Fluxophy_Widget');
+    }
+
+?>
---
+++ fluxophy/readme.txt
@@ -0,0 +1,31 @@
+=== Fluxophy ===
+Contributors:
+Donate link:
+Tags: fluxophy, json, stream, feed, widget, facebook, external source
+Requires at least: 3.3.1
+Tested up to: 3.8.1
+Stable tag: 0.5.0
+
+This plugin fetches external JSON sources and displays them in a widget
+
+
+== Description ==
+
+This plugin fetches external JSON sources and displays them in a widget. For example, you can show facebook feeds with URL http://www.facebook.com/feeds/page.php?format=json&id=YOUR_ID
+
+== Installation ==
+
+For WordPress 3.+:
+
+1. Download the last version of the plugin from its page.
+1. Upload the .zip through the Plugins/Add new/Upload of the WordPress administrator.
+1. Activate the plugin through the Plugins menu of the WordPress administrator.
+1. Pull the widget named Fluxophy into your sidebar.
+
+
+= What are the options for this plug in? =
+
+1. Title: This is what appears on the widget.  Default: Fluxophy
+1. Source URL: Address where the data will be fetched from
+1. Display Count: Number of entries to show
+
