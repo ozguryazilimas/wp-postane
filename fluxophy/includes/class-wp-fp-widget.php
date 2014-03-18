e0df7ab265005e22ded9e26ba1bd16c55ba96346
Onur Küçük <onur@delipenguen.net>
2014-03-19 00:21:09 +0200

    refs #10278 fp move home button to top, make anchors open in new tab

--- fluxophy/includes/class-wp-fp-widget.php
+++ fluxophy/includes/class-wp-fp-widget.php
@@ -111,12 +111,8 @@ class WP_Fluxophy_Widget extends WP_Widget {
 
 function fluxophy_display_data($source_url, $display_count, $picture_url, $account_name, $account_link) {
     $output = '';
-    $output .= '<ul id="fluxophy_widget">';
-    $output .= fluxophy_fetch_data_fb($source_url, $display_count, $picture_url);
-    $output .= '</ul>';
-
     $output .= '<div>';
-    $output .= '<a href="'. $account_link . '" class="fluxophy_button_home">';
+    $output .= '<a href="'. $account_link . '" class="fluxophy_button_home" target="_blank">';
     $output .= '<span class="fluxophy_button_home_icon">';
     $output .= '<svg viewBox="0 0 33 33" width="25" height="25" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><path d="M 17.996,32L 12,32 L 12,16 l-4,0 l0-5.514 l 4-0.002l-0.006-3.248C 11.993,2.737, 13.213,0, 18.512,0l 4.412,0 l0,5.515 l-2.757,0 c-2.063,0-2.163,0.77-2.163,2.209l-0.008,2.76l 4.959,0 l-0.585,5.514L 18,16L 17.996,32z"></path></g></svg>';
     $output .= '</span>';
@@ -125,6 +121,10 @@ function fluxophy_display_data($source_url, $display_count, $picture_url, $accou
     $output .= '</span>';
     $output .= '</a>';
     $output .= '</div>';
+
+    $output .= '<ul id="fluxophy_widget">';
+    $output .= fluxophy_fetch_data_fb($source_url, $display_count, $picture_url);
+    $output .= '</ul>';
 
     return $output;
 }
@@ -144,7 +144,7 @@ function fluxophy_fetch_data_fb($source_url, $display_count, $picture_url) {
                 $output .= '<img src="' . $picture_url . '"></img>';
             }
 
-            $output .= '<a href="' . $received->entries[$i]->alternate . '" title="' . date_i18n('G:i - d F Y', strtotime($received->entries[$i]->published)) . '">';
+            $output .= '<a href="' . $received->entries[$i]->alternate . '" title="' . date_i18n('G:i - d F Y', strtotime($received->entries[$i]->published)) . '" target="_blank">';
             $output .= date_i18n('d M', strtotime($received->entries[$i]->published));
             $output .= '</a>';
 
