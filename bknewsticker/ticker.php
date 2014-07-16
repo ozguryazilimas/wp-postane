10034c83edb0dd5eac1ff0261de4b8fce2691bcf
Onur Küçük <onur@delipenguen.net>
2014-07-16 12:03:47 +0300

    refs #12244 make newsticker height 25px, give some top padding to news

--- bknewsticker/ticker.php
+++ bknewsticker/ticker.php
@@ -63,7 +63,7 @@ function insert_bknewsticker(){
 		jQuery('#bknewsticker').cycle({ 
 			speed: <?php echo $tickerspeed; ?>000,
 			timeout: <?php echo $tickertimeout; ?>000,
-			height: 20,
+			height: 25,
 			fx: '<?php echo $tickeranimation; ?>',
 			pause: 1,
 			containerResize: 1
