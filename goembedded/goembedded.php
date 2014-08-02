925b71d00b9d5b1f6f4dd732fe61720360441b64
Onur Küçük <onur@delipenguen.net>
2014-08-03 00:42:14 +0300

    refs #11111 initial add for goembedded, activates oEmbed for comments

---
+++ goembedded/goembedded.php
@@ -0,0 +1,69 @@
+<?php
+/*
+Plugin Name: Goembedded
+Plugin URI: http://www.ozguryazilim.com.tr
+Description: This plugin activates Wordpress oEmbed capabilities for comments.
+Version: 0.1.0
+Author: Onur Küçük
+Author URI: http://www.delipenguen.net
+License: GPL2
+*/
+
+/*  Copyright (C) 2014, Onur Küçük
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+class GE_OEmbed_Comments {
+  function __construct() {
+    add_action( 'init', array($this, 'init'));
+  }
+
+  function init() {
+    if (!is_admin()) {
+      $this->add_oembed_comment_filter();
+    }
+  }
+
+  /*
+   * filter for oEmbed in comments
+   */
+  function add_oembed_comment_filter() {
+    // make_clickable breaks oEmbed regex, try to activate before that
+    $has_clickable = has_filter('get_comment_text', 'make_clickable');
+    $priority = $has_clickable ? ($has_clickable - 1) : 8;
+
+    add_filter('get_comment_text', array($this, 'oembed_filter'), $priority);
+  }
+
+  /*
+   * add the comment filter
+   */
+  function oembed_filter($comment_text) {
+    global $wp_embed;
+
+    // Automatic discovery might be a security risk
+    add_filter('embed_oembed_discover', '__return_false', 999);
+    $comment_text = $wp_embed->autoembed($comment_text);
+
+    // reenable automatic discovery
+    remove_filter('embed_oembed_discover', '__return_false', 999);
+
+    return $comment_text;
+  }
+}
+
+new GE_OEmbed_Comments;
+
+?>
---
+++ goembedded/readme.txt
@@ -0,0 +1,34 @@
+=== Goembedded ===
+Contributors:
+Donate link:
+Tags: goembedded, video, multimedia, video list, embed, embedded, oembed, comments
+Requires at least: 3.5.1
+Tested up to: 3.9.1
+Stable tag: 0.1.0
+
+This plugin activates Wordpress oEmbed capabilities for comments.
+
+
+== Description ==
+
+This plugin activates Wordpress oEmbed capabilities for comments.
+
+
+== Installation ==
+
+For WordPress 3.+:
+
+1. Download the latest version of the plugin from its page.
+1. Upload the .zip through the Plugins/Add new/Upload of the WordPress administrator.
+1. Activate the plugin through the Plugins menu of the WordPress administrator.
+
+This plug in will work as is.
+
+
+== Frequently Asked Questions ==
+
+= Is this plugin free ? =
+
+Goembedded is licensed under GNU GPLv2, see http://www.gnu.org/licenses/gpl-2.0.html for more details.
+
+
