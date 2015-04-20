dbf0a7984ae5c97eb7c1bbd5b6577100421fc2bb
Onur Küçük <onur@delipenguen.net>
2015-04-20 22:20:02 +0300

    refs #15960 update google-analytics-for-wordpress from 5.3.3 to 5.4

--- google-analytics-for-wordpress/vendor/autoload.php
+++ google-analytics-for-wordpress/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit4bdc500f9ad408badfeb53112af5d59f::getLoader();
+return ComposerAutoloaderInitc25dd8530d96371aeae7c93f0fbd7210::getLoader();
--- google-analytics-for-wordpress/vendor/autoload_52.php
+++ google-analytics-for-wordpress/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInitc7697842af642d0524332c8e2aa408d0::getLoader();
+return ComposerAutoloaderInitf47bb2769d4e49c246453941d671b543::getLoader();
