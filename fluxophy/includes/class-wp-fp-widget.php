b6ef4af1c6a9807dbcdfe9f812c93b01edfe08bb
Onur Küçük <onur@delipenguen.net>
2015-08-29 19:23:06 +0300

    refs #17238 show fb and twitter in the same widget for fluxophy

--- fluxophy/includes/class-wp-fp-widget.php
+++ fluxophy/includes/class-wp-fp-widget.php
@@ -44,13 +44,23 @@ class WP_Fluxophy_Widget extends WP_Widget {
     $picture_url = $instance['picture_url'];
     $account_name = $instance['account_name'];
     $account_link = $instance['account_link'];
+    $twitter_account = $instance['twitter_account'];
+    $twitter_data_id = $instance['twitter_data_id'];
 
     $output .= $before_widget;
     if ($title && $title != '') {
       $output .= $before_title . $title . $after_title;
     }
 
-    $output .= fluxophy_display_data($source_url, $display_count, $picture_url, $account_name, $account_link);
+    $output .= fluxophy_display_data(
+      $source_url,
+      $display_count,
+      $picture_url,
+      $account_name,
+      $account_link,
+      $twitter_account,
+      $twitter_data_id
+    );
 
     $output .= $after_widget;
     echo $output;
@@ -64,6 +74,8 @@ class WP_Fluxophy_Widget extends WP_Widget {
     $instance['account_name'] = strip_tags($new_instance['account_name']);
     $instance['account_link'] = strip_tags($new_instance['account_link']);
     $instance['display_count'] = $new_instance['display_count'];
+    $instance['twitter_account'] = strip_tags($new_instance['twitter_account']);
+    $instance['twitter_data_id'] = strip_tags($new_instance['twitter_data_id']);
 
     return $instance;
   }
@@ -78,6 +90,8 @@ class WP_Fluxophy_Widget extends WP_Widget {
     $picture_url = (isset($instance['picture_url'])) ? strip_tags($instance['picture_url']) : '';
     $account_name = (isset($instance['account_name'])) ? strip_tags($instance['account_name']) : '';
     $account_link = (isset($instance['account_link'])) ? strip_tags($instance['account_link']) : '';
+    $twitter_account = (isset($instance['twitter_account'])) ? strip_tags($instance['twitter_account']) : '';
+    $twitter_data_id = (isset($instance['twitter_data_id'])) ? strip_tags($instance['twitter_data_id']) : '';
     $display_count = empty($instance['display_count']) ? FLUXOPHY_DISPLAY_COUNT: (int) $instance['display_count'];
 
 ?>
@@ -105,13 +119,23 @@ class WP_Fluxophy_Widget extends WP_Widget {
         <label for="<?php echo $this->get_field_id('display_count'); ?>"><?php _e('Number of entries to show (default 10):', 'fluxophy') ?></label>
         <input id="<?php echo $this->get_field_id('display_count'); ?>" name="<?php echo $this->get_field_name('display_count'); ?>" type="text" value="<?php echo $display_count; ?>" size="3" />
       </p>
+      <p>
+        <label for="<?php echo $this->get_field_id('twitter_account'); ?>"><?php _e('Twitter Account:', 'fluxophy'); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id('twitter_account'); ?>" name="<?php echo $this->get_field_name('twitter_account'); ?>" type="text" value="<?php echo esc_attr($twitter_account); ?>" />
+      </p>
+      <p>
+        <label for="<?php echo $this->get_field_id('twitter_data_id'); ?>"><?php _e('Twitter Data ID:', 'fluxophy'); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id('twitter_data_id'); ?>" name="<?php echo $this->get_field_name('twitter_data_id'); ?>" type="text" value="<?php echo esc_attr($twitter_data_id); ?>" />
+      </p>
 <?php
   }
 }
 
 
-function fluxophy_display_data($source_url, $display_count, $picture_url, $account_name, $account_link) {
+function fluxophy_display_data($source_url, $display_count, $picture_url, $account_name, $account_link, $twitter_account, $twitter_data_id) {
   $output = '';
+  /* old style fb
+   *
   $output .= '<div>';
   $output .= '<a href="'. $account_link . '" class="fluxophy_button_home" target="_blank">';
   $output .= '<span class="fluxophy_button_home_icon">';
@@ -122,10 +146,45 @@ function fluxophy_display_data($source_url, $display_count, $picture_url, $accou
   $output .= '</span>';
   $output .= '</a>';
   $output .= '</div>';
+   */
 
+  $output .= '<div>';
+  $output .= '<div id="fluxophy_button_show_twitter">';
+  $output .= '<span class="fluxophy_button_home fluxophy_button_home_twitter">';
+  $output .= '<svg viewBox="0 0 512 512" width="25" height="25" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><path d="M419.6 168.6c-11.7 5.2-24.2 8.7-37.4 10.2 13.4-8.1 23.8-20.8 28.6-36 -12.6 7.5-26.5 12.9-41.3 15.8 -11.9-12.6-28.8-20.6-47.5-20.6 -42 0-72.9 39.2-63.4 79.9 -54.1-2.7-102.1-28.6-134.2-68 -17 29.2-8.8 67.5 20.1 86.9 -10.7-0.3-20.7-3.3-29.5-8.1 -0.7 30.2 20.9 58.4 52.2 64.6 -9.2 2.5-19.2 3.1-29.4 1.1 8.3 25.9 32.3 44.7 60.8 45.2 -27.4 21.4-61.8 31-96.4 27 28.8 18.5 63 29.2 99.8 29.2 120.8 0 189.1-102.1 185-193.6C399.9 193.1 410.9 181.7 419.6 168.6z"></path></g></svg>';
+  $output .= '</span>';
+  $output .= '</div>';
+  $output .= '<div id="fluxophy_button_show_fb" class="active">';
+  $output .= '<span class="fluxophy_button_home fluxophy_button_home_fb">';
+  $output .= '<svg viewBox="0 0 33 33" width="25" height="25" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><path d="M 17.996,32L 12,32 L 12,16 l-4,0 l0-5.514 l 4-0.002l-0.006-3.248C 11.993,2.737, 13.213,0, 18.512,0l 4.412,0 l0,5.515 l-2.757,0 c-2.063,0-2.163,0.77-2.163,2.209l-0.008,2.76l 4.959,0 l-0.585,5.514L 18,16L 17.996,32z"></path></g></svg>';
+  $output .= '</span>';
+  $output .= '</div>';
+  $output .= '</div>';
+
+  $output .= '<div id="fluxophy_widget_fb">';
   $output .= '<ul id="fluxophy_widget">';
   $output .= fluxophy_fetch_data_fb($source_url, $display_count, $picture_url);
   $output .= '</ul>';
+  $output .= '</div>';
+
+  $output .= '<div id="fluxophy_widget_twitter" style="display:none;">';
+  $output .= fluxophy_fetch_data_twitter($twitter_account, $twitter_data_id);
+  $output .= '</div>';
+
+  $output .= "<script>
+    jQuery('#fluxophy_button_show_fb').on('click', function() {
+      jQuery('#fluxophy_button_show_twitter').removeClass('active');
+      jQuery('#fluxophy_button_show_fb').addClass('active');
+      jQuery('#fluxophy_widget_twitter').hide();
+      jQuery('#fluxophy_widget_fb').show();
+    });
+    jQuery('#fluxophy_button_show_twitter').on('click', function() {
+      jQuery('#fluxophy_button_show_fb').removeClass('active');
+      jQuery('#fluxophy_button_show_twitter').addClass('active');
+      jQuery('#fluxophy_widget_fb').hide();
+      jQuery('#fluxophy_widget_twitter').show();
+    });
+    </script>";
 
   return $output;
 }
@@ -159,6 +218,13 @@ function fluxophy_fetch_data_fb($source_url, $display_count, $picture_url) {
       }
     }
   }
+
+  return $output;
+}
+
+function fluxophy_fetch_data_twitter($account, $data_id) {
+  $output = '<a class="twitter-timeline" href="https://twitter.com/' . $account . '" height="360" data-widget-id="' . $data_id . '">@22dakika kullanıcısından Tweetler</a>' .
+    '<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
 
   return $output;
 }
--- fluxophy/readme.txt
+++ fluxophy/readme.txt
@@ -3,8 +3,8 @@ Contributors:
 Donate link:
 Tags: fluxophy, json, stream, feed, widget, facebook, external source
 Requires at least: 3.3.1
-Tested up to: 4.1.0
-Stable tag: 1.0.0
+Tested up to: 4.2.4
+Stable tag: 2.0.0
 
 This plugin fetches external JSON sources and displays them in a widget
 
