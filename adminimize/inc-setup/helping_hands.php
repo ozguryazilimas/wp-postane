399780badc93766efd33712ce84c8c902677d0cf
Onur Küçük <onur@delipenguen.net>
2016-01-26 12:03:48 +0200

    refs #18438 update adminimize from 1.9.0 to 1.9.1

--- adminimize/inc-setup/helping_hands.php
+++ adminimize/inc-setup/helping_hands.php
@@ -3,7 +3,7 @@
  * @package    Adminimize
  * @subpackage Helping_Functions
  * @author     Frank Bültge <frank@bueltge.de
- * @since      2015-03-19
+ * @since      2016-01-22
  */
 if ( ! function_exists( 'add_action' ) ) {
 	die( "Hi there!  I'm just a part of plugin, not much I can do when called directly." );
@@ -53,41 +53,49 @@ function _mw_adminimize_in_arrays( $array1, $array2 ) {
 	return (bool) count( array_intersect( $array1, $array2 ) );
 }
 
-// fix some badly enqueued scripts with no sense of HTTPS
+// Fix some badly enqueued scripts with no sense of HTTPS.
 // Kudos to http://snippets.webaware.com.au/snippets/cleaning-up-wordpress-plugin-script-and-stylesheet-loads-over-ssl/
 add_action( 'wp_print_scripts', '_mw_adminimize_enqueueScriptsFix', 100 );
 add_action( 'wp_print_styles', '_mw_adminimize_enqueueStylesFix', 100 );
 
 /**
- * force plugins to load scripts with SSL if page is SSL
+ * Force plugins to load scripts with SSL if page is SSL.
  */
 function _mw_adminimize_enqueueScriptsFix() {
 
-	if ( ! is_admin() ) {
-		if ( ! empty( $_SERVER[ 'HTTPS' ] ) ) {
-			global $wp_scripts;
-			foreach ( (array) $wp_scripts->registered as $script ) {
-				if ( stripos( $script->src, 'http://', 0 ) !== FALSE ) {
-					$script->src = str_replace( 'http://', 'https://', $script->src );
-				}
-			}
+	if ( is_admin() ) {
+		return;
+	}
+
+	$https_values = array( NULL, 'off' );
+	if ( ! isset( $_SERVER[ 'HTTPS' ] ) || in_array( $_SERVER[ 'HTTPS' ], $https_values ) ) {
+		return;
+	}
+
+	foreach ( (array) $GLOBALS[ 'wp_scripts' ]->registered as $script ) {
+		if ( FALSE !== stripos( $script->src, 'http://', 0 ) ) {
+			$script->src = str_replace( 'http://', 'https://', $script->src );
 		}
 	}
 }
 
 /**
- * force plugins to load styles with SSL if page is SSL
+ * Force plugins to load styles with SSL if page is SSL.
  */
 function _mw_adminimize_enqueueStylesFix() {
 
-	if ( ! is_admin() ) {
-		if ( ! empty( $_SERVER[ 'HTTPS' ] ) ) {
-			global $wp_styles;
-			foreach ( (array) $wp_styles->registered as $script ) {
-				if ( stripos( $script->src, 'http://', 0 ) !== FALSE ) {
-					$script->src = str_replace( 'http://', 'https://', $script->src );
-				}
-			}
+	if ( is_admin() ) {
+		return;
+	}
+
+	$https_values = array( NULL, 'off' );
+	if ( ! isset( $_SERVER[ 'HTTPS' ] ) || in_array( $_SERVER[ 'HTTPS' ], $https_values ) ) {
+		return;
+	}
+
+	foreach ( (array) $GLOBALS[ 'wp_styles' ]->registered as $script ) {
+		if ( FALSE !== stripos( $script->src, 'http://', 0 ) ) {
+			$script->src = str_replace( 'http://', 'https://', $script->src );
 		}
 	}
 }
