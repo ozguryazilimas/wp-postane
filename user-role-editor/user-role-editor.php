8e9109bfa6110a831330150ecc049a839b09527d
Onur Küçük <onur@delipenguen.net>
2017-03-09 14:41:02 +0300

    refs #23159 update user-role-editor from 4.31.1 to 4.32.1

--- user-role-editor/user-role-editor.php
+++ user-role-editor/user-role-editor.php
@@ -3,7 +3,7 @@
 Plugin Name: User Role Editor
 Plugin URI: https://www.role-editor.com
 Description: Change/add/delete WordPress user roles and capabilities.
-Version: 4.31.1
+Version: 4.32.1
 Author: Vladimir Garagulya
 Author URI: https://www.role-editor.com
 Text Domain: ure
@@ -11,7 +11,7 @@ Domain Path: /lang/
 */
 
 /*
-Copyright 2010-2016  Vladimir Garagulya  (email: support@role-editor.com)
+Copyright 2010-2017  Vladimir Garagulya  (email: support@role-editor.com)
 */
 
 if (!function_exists('get_option')) {
@@ -23,7 +23,7 @@ if (defined('URE_PLUGIN_URL')) {
    wp_die('It seems that other version of User Role Editor is active. Please deactivate it before use this version');
 }
 
-define('URE_VERSION', '4.31.1');
+define('URE_VERSION', '4.32.1');
 define('URE_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('URE_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('URE_PLUGIN_BASE_NAME', plugin_basename(__FILE__));
@@ -47,4 +47,4 @@ Ure_Lib::check_version(get_bloginfo('version'), $ure_required_wp_version, $exit_
 
 require_once(URE_PLUGIN_DIR .'includes/loader.php');
 
-$GLOBALS['user_role_editor'] = new User_Role_Editor();
+$GLOBALS['user_role_editor'] = User_Role_Editor::get_instance();
