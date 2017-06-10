544da9db1235f97cfa2e1d086a6bd5976091b80f
Onur Küçük <onur@delipenguen.net>
2017-06-10 19:58:53 +0300

    refs #24147 update google-analytics-dashboard-for-wp from 5.0.0.1 to 5.0.1

--- google-analytics-dashboard-for-wp/front/js/tracking-tagmanager-events.js
+++ google-analytics-dashboard-for-wp/front/js/tracking-tagmanager-events.js
@@ -1,1 +1,1 @@
-(function(a){a(window).load(function(){a.scrollDepth({percentage:true,userTiming:false,pixelDepth:false,gtmOverride:false})})})(jQuery);
+(function(a){a(window).on("load",function(){a.scrollDepth({percentage:true,userTiming:false,pixelDepth:false,gtmOverride:false})})})(jQuery);
