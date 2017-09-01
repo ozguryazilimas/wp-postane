820f56c9e3d559fa8edc54669e0a02ad049a6578
Onur Küçük <onur@delipenguen.net>
2017-09-01 13:32:59 +0300

    refs #25054 update captcha from 4.3.1 to 4.3.3

--- captcha/js/front_end_script.js
+++ captcha/js/front_end_script.js
@@ -1,35 +1,35 @@
 ( function( $ ) {
-	$( document ).on( "click", '.cptch_reload_button, .wpcf7-submit', function() {
-		cptch_reload( $( this ) );
+	$( document ).on( "click", '.hctpc_reload_button', function() { //, .wpcf7-submit
+		hctpc_reload( $( this ) );
 	}).on( "touchstart", function( event ) {
 		event = event || window.event;
 		var item = $( event.target );
-		if ( cptch_vars.enlarge == '1' ) {
+		if ( hctpc_vars.enlarge == '1' ) {
 			var element = $( event.target );
-			if ( element.hasClass( 'cptch_img' ) ) {
+			if ( element.hasClass( 'hctpc_img' ) ) {
 				event.preventDefault();
-				element.toggleClass( 'cptch_reduce' );
-				$( '.cptch_img' ).not( element ).removeClass( 'cptch_reduce' );
+				element.toggleClass( 'hctpc_reduce' );
+				$( '.hctpc_img' ).not( element ).removeClass( 'hctpc_reduce' );
 			} else {
-				$( '.cptch_img' ).removeClass( 'cptch_reduce' );
+				$( '.hctpc_img' ).removeClass( 'hctpc_reduce' );
 			}
 		}
-		if ( item.hasClass( 'cptch_reload_button' ) || item.attr( 'name' ) == 'ac_form_submit' )
-			cptch_reload( item );
+		if ( item.hasClass( 'hctpc_reload_button' ) || item.attr( 'name' ) == 'ac_form_submit' )
+			hctpc_reload( item );
 	}).ready( function() {
-		if ( $( '.cptch_recognition' ).length ) {
-			$( '.cptch_recognition' ).each( function() {				
-				$( this ).find( '.cptch_input' ).css( 'width', $( this ).find( '.cptch_images_wrap' ).css( 'width' ) );
+		if ( $( '.hctpc_recognition' ).length ) {
+			$( '.hctpc_recognition' ).each( function() {				
+				$( this ).find( '.hctpc_input' ).css( 'width', $( this ).find( '.hctpc_images_wrap' ).css( 'width' ) );
 			} );
 		}
 
-		var ajax_containers = $( '.cptch_ajax_wrap' );
+		var ajax_containers = $( '.hctpc_ajax_wrap' );
 
 		if ( ! ajax_containers.length )
 			return;
 
 		ajax_containers.each( function() {
-			cptch_reload( $( this ), true );
+			hctpc_reload( $( this ), true );
 		});
 	});
 })(jQuery);
@@ -37,43 +37,43 @@
 /**
  * Reload captcha
  */
-function cptch_reload( object, is_ajax_load ) {
+function hctpc_reload( object, is_ajax_load ) {
 	is_ajax_load = is_ajax_load || false;
 	(function($) {
 		if  ( is_ajax_load ) {
 			var captcha = object;
 		} else {
-			var captcha = object.hasClass( 'cptch_reload_button' ) ? object.parent().parent( '.cptch_wrap' ) : object.closest( 'form' ).find( '.cptch_wrap' );
+			var captcha = object.hasClass( 'hctpc_reload_button' ) ? object.parent().parent( '.hctpc_wrap' ) : object.closest( 'form' ).find( '.hctpc_wrap' );
 		}
-		var button  = captcha.find( '.cptch_reload_button' );
-		if ( ! captcha.length || button.hasClass( 'cptch_active' ) )
+		var button  = captcha.find( '.hctpc_reload_button' );
+		if ( ! captcha.length || button.hasClass( 'hctpc_active' ) )
 			return false;
-		button.addClass( 'cptch_active' );
+		button.addClass( 'hctpc_active' );
 		var captcha_block = captcha.parent(),
 			input         = captcha.find( 'input:text' ),
-			input_name    = is_ajax_load ? captcha.attr( 'data-cptch-input' ) : input.attr( 'name' ),
-			input_class   = is_ajax_load ? captcha.attr( 'data-cptch-class' ) : input.attr( 'class' ).replace( /cptch_input/, '' ).replace( /^\s+|\s+$/g, '' ),
-			form_slug     = is_ajax_load ? captcha.attr( 'data-cptch-form' ) : captcha_block.find( 'input[name="cptch_form"]' ).val();
+			input_name    = is_ajax_load ? captcha.attr( 'data-hctpc-input' ) : input.attr( 'name' ),
+			input_class   = is_ajax_load ? captcha.attr( 'data-hctpc-class' ) : input.attr( 'class' ).replace( /hctpc_input/, '' ).replace( /^\s+|\s+$/g, '' ),
+			form_slug     = is_ajax_load ? captcha.attr( 'data-hctpc-form' ) : captcha_block.find( 'input[name="hctpc_form"]' ).val();
 		$.ajax({
 			type: 'POST',
-			url: cptch_vars.ajaxurl,
+			url: hctpc_vars.ajaxurl,
 			data: {
-				action:              'cptch_reload',
-				cptch_nonce:       cptch_vars.nonce,
-				cptch_input_name:  input_name,
-				cptch_input_class: input_class,
-				cptch_form_slug:   form_slug
+				action:              'hctpc_reload',
+				hctpc_nonce:       hctpc_vars.nonce,
+				hctpc_input_name:  input_name,
+				hctpc_input_class: input_class,
+				hctpc_form_slug:   form_slug
 			},
 			success: function( result ) {
-				captcha_block.find( '.cptch_to_remove' ).remove();
+				captcha_block.find( '.hctpc_to_remove' ).remove();
 				if ( input_class === '' )
 					captcha.replaceWith( result ); /* for default forms */
 				else
 					captcha_block.replaceWith( result ); /* for custom forms */
 
-				if ( $( result ).hasClass( 'cptch_recognition' ) || $( result ).find( '.cptch_wrap' ).hasClass( 'cptch_recognition' ) ) {
-					$( '.cptch_recognition' ).each( function() {				
-						$( this ).find( '.cptch_input' ).css( 'width', $( this ).find( '.cptch_images_wrap' ).css( 'width' ) );
+				if ( $( result ).hasClass( 'hctpc_recognition' ) || $( result ).find( '.hctpc_wrap' ).hasClass( 'hctpc_recognition' ) ) {
+					$( '.hctpc_recognition' ).each( function() {				
+						$( this ).find( '.hctpc_input' ).css( 'width', $( this ).find( '.hctpc_images_wrap' ).css( 'width' ) );
 					} );
 				}
 			},
--- captcha/js/script.js
+++ captcha/js/script.js
@@ -9,18 +9,18 @@
 		 * Handle the styling of the "Settings" tab on the plugin settings page
 		 * @since 4.2.3
 		 */
-		var limitOption	= $( '#cptch_enable_time_limit' ),
-			imageFormat	= $( '#cptch_operand_format_images' );
+		var limitOption	= $( '#hctpc_enable_time_limit' ),
+			imageFormat	= $( '#hctpc_operand_format_images' );
 
 		/*
 		* Hide "time limit thershold" field under unchecked "time limit" field
 		*/
 		if ( ! $( limitOption ).is( ':checked' ) ) {
-			$( limitOption ).closest( 'tr' ).nextAll( '.cptch_time_limit' ).hide();
+			$( limitOption ).closest( 'tr' ).nextAll( '.hctpc_time_limit' ).hide();
 		}
 
 		$( limitOption ).click( function() {
-			$( limitOption ).closest( 'tr' ).nextAll( '.cptch_time_limit' ).toggle();
+			$( limitOption ).closest( 'tr' ).nextAll( '.hctpc_time_limit' ).toggle();
 		});
 
 		/*
@@ -42,55 +42,79 @@
 		});
 
 		/* Handle the displaying of notice message above lists of image packages */
-		function cptchImageOptions() {
+		function hctpcImageOptions() {
 			var isChecked = imageFormat.is( ':checked' );
 			if ( isChecked ){
-				$( '.cptch_images_options' ).show();
-				$( '.cptch_enable_to_use_several_packages' ).closest( '.bws_pro_version_bloc' ).show();
+				$( '.hctpc_images_options' ).show();
 			} else {
-				$( '.cptch_images_options' ).hide();
-				$( '.cptch_enable_to_use_several_packages' ).closest( '.bws_pro_version_bloc' ).hide();
+				$( '.hctpc_images_options' ).hide();
 			}
 		}
-		cptchImageOptions()
-		imageFormat.click( function() { cptchImageOptions(); } );
+		hctpcImageOptions()
+		imageFormat.click( function() { hctpcImageOptions(); } );
 
-		function cptch_type() {
-			if ( 'recognition' == $( 'input[name="cptch_type"]:checked' ).val() ) {
-				$( '.cptch_for_recognition' ).show();
-				$( '.cptch_for_math_actions' ).hide();
+		function hctpc_type() {
+			if ( 'recognition' == $( 'input[name="hctpc_type"]:checked' ).val() ) {
+				$( '.hctpc_for_recognition' ).show();
+				$( '.hctpc_for_math_actions' ).hide();
 				imageFormat.attr( 'checked', 'checked' );
-				cptchImageOptions();
+				hctpcImageOptions();
 			} else {
-				$( '.cptch_for_recognition' ).hide();
-				$( '.cptch_for_math_actions' ).show();
+				$( '.hctpc_for_recognition' ).hide();
+				$( '.hctpc_for_math_actions' ).show();
 			}
 		}
-		cptch_type();
-		$( 'input[name="cptch_type"]' ).click( function() { cptch_type(); } );
+		hctpc_type();
+		$( 'input[name="hctpc_type"]' ).click( function() { hctpc_type(); } );
 
 		/**
 		 * Hide/show whitelist "add new form"
 		 */
-		$( 'button[name="cptch_show_whitelist_form"]' ).click( function() {
+		$( 'button[name="hctpc_show_whitelist_form"]' ).click( function() {
 			$( this ).parent( 'form' ).hide();
-			$( '.cptch_whitelist_form' ).show();
+			$( '.hctpc_whitelist_form' ).show();
 			return false;
 		});
 
 		/*  add to whitelist my ip */
-		$( 'input[name="cptch_add_to_whitelist_my_ip"]' ).change( function() {
+		$( 'input[name="hctpc_add_to_whitelist_my_ip"]' ).change( function() {
 			if ( $( this ).is( ':checked' ) ) {
-				var my_ip = $( 'input[name="cptch_add_to_whitelist_my_ip_value"]' ).val();
-				$( 'input[name="cptch_add_to_whitelist"]' ).val( my_ip ).attr( 'readonly', 'readonly' );
+				var my_ip = $( 'input[name="hctpc_add_to_whitelist_my_ip_value"]' ).val();
+				$( 'input[name="hctpc_add_to_whitelist"]' ).val( my_ip ).attr( 'readonly', 'readonly' );
 			} else {
-				$( 'input[name="cptch_add_to_whitelist"]' ).val( '' ).removeAttr( 'readonly' );
+				$( 'input[name="hctpc_add_to_whitelist"]' ).val( '' ).removeAttr( 'readonly' );
 			}
 		});
 
-		/* Open/hide packages pro tab */
-		$( '#cptch_show_packages_pro_tab_open' ).click( function() {
-			$( '#cptch_show_packages_pro_tab' ).toggle();
-		});
+		/**
+		 * Handle the styling of the "Settings" tab on the plugin settings page
+		 */
+		var tabs = $( '#hctpc_settings_tabs_wrapper' );
+		if ( tabs.length ) {
+			var current_tab_field = $( 'input[name="hctpc_active_tab"]' ),
+				prevent_tabs_change = false,
+				active_tab = current_tab_field.val();
+			if ( '' == active_tab ) {
+				var active_tab_index = 0;
+			} else {
+				var active_tab_index = $( '#hctpc_settings_tabs li[data-slug=' + active_tab + ']' ).index();
+			}
+
+			$( '.hctpc_tab' ).css( 'min-height', $( '#hctpc_settings_tabs' ).css( 'height' ) );
+
+			/* jQuery tabs initialization */
+			tabs.tabs({
+				active: active_tab_index
+			}).on( "tabsactivate", function( event, ui ) {
+				if ( ! prevent_tabs_change ) {
+					active_tab = ui.newTab.data( 'slug' );
+					current_tab_field.val( active_tab );
+				}
+				prevent_tabs_change = false;
+			});
+			$( '.hctpc_trigger_tab_click' ).on( 'click', function () {
+				$( '#hctpc_settings_tabs a[href="' + $( this ).attr( 'href' ) + '"]' ).click();
+			});
+		}
 	});
 })(jQuery);
