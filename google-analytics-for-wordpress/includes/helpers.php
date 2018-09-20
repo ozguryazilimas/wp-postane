0df573363ac13f46a8f1c3826d2fbe1555a53898
Onur Küçük <onur@delipenguen.net>
2018-09-20 16:19:58 +0300

    refs #30344 update google-analytics-for-wordpress from 7.1.0 to 7.2.0

--- google-analytics-for-wordpress/includes/helpers.php
+++ google-analytics-for-wordpress/includes/helpers.php
@@ -845,7 +845,7 @@ function monsterinsights_get_country_list( $translated = false ) {
 }
 
 function monsterinsights_get_api_url(){
-	return apply_filters( 'monsterinsights_get_api_url', 'www.monsterinsights.com/v1/' );
+	return apply_filters( 'monsterinsights_get_api_url', 'api.monsterinsights.com/v2/' );
 }
 
 function monsterinsights_get_licensing_url(){
@@ -983,3 +983,27 @@ if ( ! function_exists ( 'remove_class_action' ) ) {
 		remove_class_filter( $tag, $class_name, $method_name, $priority );
 	}
 } // End function exists
+
+/**
+ * Format a big number, instead of 1000000 you get 1.0M, works with billions also.
+ *
+ * @param int $number
+ * @param int $precision
+ *
+ * @return string
+ */
+function monsterinsights_round_number( $number, $precision = 2 ) {
+
+	if ( $number < 1000000 ) {
+		// Anything less than a million
+		$number = number_format_i18n( $number );
+	} else if ( $number < 1000000000 ) {
+		// Anything less than a billion
+		$number = number_format_i18n( $number / 1000000, $precision ) . 'M';
+	} else {
+		// At least a billion
+		$number = number_format_i18n( $number / 1000000000, $precision ) . 'B';
+	}
+
+	return $number;
+}
