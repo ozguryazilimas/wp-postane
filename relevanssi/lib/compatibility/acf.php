d9e27624bbc6753abd2c33ce107d692d343a6789
Onur Küçük <onur@delipenguen.net>
2019-09-19 12:18:21 +0300

    refs #35101 update relevanssi from 4.3.2 to 4.3.3

--- relevanssi/lib/compatibility/acf.php
+++ relevanssi/lib/compatibility/acf.php
@@ -39,37 +39,74 @@ function relevanssi_acf_relationship_fields( $search_ok ) {
  * @param int    $post_id     The post ID.
  * @param string $field_name  Name of the field.
  * @param string $field_value The field value.
+ *
+ * @return int Number of tokens indexed.
  */
 function relevanssi_index_acf( &$insert_data, $post_id, $field_name, $field_value ) {
 	if ( ! is_admin() ) {
 		include_once ABSPATH . 'wp-admin/includes/plugin.php'; // Otherwise is_plugin_active() will cause a fatal error.
 	}
 	if ( ! function_exists( 'is_plugin_active' ) ) {
-		return;
+		return 0;
 	}
 	if ( ! is_plugin_active( 'advanced-custom-fields/acf.php' ) && ! is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) ) {
-		return;
+		return 0;
 	}
 	if ( ! function_exists( 'get_field_object' ) ) {
-		return; // ACF is active, but not loaded.
+		return 0; // ACF is active, but not loaded.
 	}
 
 	$field_object = get_field_object( $field_name, $post_id );
 	if ( ! isset( $field_object['choices'] ) ) {
-		return; // Not a "select" field.
+		return 0; // Not a "select" field.
 	}
 	if ( is_array( $field_value ) ) {
-		return; // Not handled (currently).
+		return 0; // Not handled (currently).
 	}
 	if ( ! isset( $field_object['choices'][ $field_value ] ) ) {
-		return; // Value does not exist.
+		return 0; // Value does not exist.
 	}
 
-	$value = $field_object['choices'][ $field_value ];
-	if ( $value ) {
-		if ( ! isset( $insert_data[ $value ]['customfield'] ) ) {
-			$insert_data[ $value ]['customfield'] = 0;
+	$n = 0;
+
+	/**
+	 * Filters the field value before it is used to save the insert data.
+	 *
+	 * The value is used as an array key, so it needs to be an integer or a
+	 * string. If your custom field values are arrays or objects, use this
+	 * filter hook to convert them into strings.
+	 *
+	 * @param any    The ACF field value.
+	 * @param string The ACF field name.
+	 * @param int    The post ID.
+	 *
+	 * @return string|int The field value.
+	 */
+	$value = apply_filters(
+		'relevanssi_acf_field_value',
+		$field_object['choices'][ $field_value ],
+		$field_name,
+		$post_id
+	);
+
+	if ( $value && ( is_integer( $value ) || is_string( $value ) ) ) {
+		$min_word_length = get_option( 'relevanssi_min_word_length', 3 );
+
+		/** This filter is documented in lib/indexing.php */
+		$value_tokens = apply_filters( 'relevanssi_indexing_tokens', relevanssi_tokenize( $value, true, $min_word_length ), 'custom_field' );
+		foreach ( $value_tokens as $token => $count ) {
+			$n++;
+			if ( ! isset( $insert_data[ $token ]['customfield'] ) ) {
+				$insert_data[ $token ]['customfield'] = 0;
+			}
+			$insert_data[ $token ]['customfield'] += $count;
+
+			// Premium indexes more detail about custom fields.
+			if ( function_exists( 'relevanssi_customfield_detail' ) ) {
+				$insert_data = relevanssi_customfield_detail( $insert_data, $token, $count, $field_name );
+			}
 		}
-		$insert_data[ $value ]['customfield']++;
 	}
+
+	return $n;
 }
