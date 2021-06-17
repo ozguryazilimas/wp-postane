39e4928ddab3c8465c8f6ec81838a057a01f8d61
Onur Küçük <onur@delipenguen.net>
2021-06-17 22:42:06 +0300

    refs #49813 update theme-check from 20200922.1 to 20210617

--- theme-check/checks/content-width.php
+++ theme-check/checks/content-width.php
@@ -5,19 +5,27 @@ class ContentWidthCheck implements themecheck {
 
 	function check( $php_files, $css_files, $other_files ) {
 
-		$ret = true;
-
 		// combine all the php files into one string to make it easier to search
 		$php = implode( ' ', $php_files );
 		checkcount();
-		if ( strpos( $php, '$content_width' ) === false && strpos( $php, '$GLOBALS' . "['content_width']" ) === false && !preg_match( '/add_filter\(\s?("|\')embed_defaults/', $php ) && !preg_match( '/add_filter\(\s?("|\')content_width/', $php ) ) {
-			$this->error[] = '<span class="tc-lead tc-required">'.__('REQUIRED','theme-check').'</span>: '.__('No content width has been defined. Example: <pre>if ( ! isset( $content_width ) ) $content_width = 900;</pre>', 'theme-check' );
-			$ret = false;
+		if (
+			strpos( $php, '$content_width' ) === false &&
+			strpos( $php, '$GLOBALS' . "['content_width']" ) === false &&
+			! preg_match( '/add_filter\(\s?("|\')embed_defaults/', $php ) &&
+			! preg_match( '/add_filter\(\s?("|\')content_width/', $php )
+		) {
+			$this->error[] = sprintf(
+				'<span class="tc-lead tc-recommended">%s</span>: %s',
+				__( 'RECOMMENDED', 'theme-check' ),
+				__( 'No content width has been defined. Example: <pre>if ( ! isset( $content_width ) ) $content_width = 900;</pre>', 'theme-check' )
+			);
 		}
 
-		return $ret;
+		return true;
 	}
 
-	function getError() { return $this->error; }
+	function getError() {
+		return $this->error;
+	}
 }
-$themechecks[] = new ContentWidthCheck;
+$themechecks[] = new ContentWidthCheck();
