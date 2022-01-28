cac73d4bfda267f5ef06fbd9f7e5644a0c7655f6
Onur Küçük <onur@delipenguen.net>
2022-01-28 13:50:20 +0300

    refs #57163 update relevanssi from 4.14.6 to 4.14.7

--- relevanssi/lib/tabs/overview-tab.php
+++ relevanssi/lib/tabs/overview-tab.php
@@ -68,9 +68,16 @@ function relevanssi_overview_tab() {
 		</td>
 	</tr>
 	<tr>
+		<th scope="row"><?php esc_html_e( 'Relevanssi Live Ajax Search', 'relevanssi' ); ?></th>
+		<td>
+		<?php // Translators: %1$s opens the link, %2$s closes it. ?>
+			<p><?php printf( esc_html__( 'If you want a live search results, you can use the Relevanssi Live Ajax Search plugin. %1$sYou can find it in the plugin repository%2$s. It will make your search forms show instant results, powered by Relevanssi.', 'relevanssi' ), "<a href='https://wordpress.org/plugins/relevanssi-live-ajax-search/'>", '</a>' ); ?></p>
+		</td>
+	</tr>
+	<tr>
 		<th scope="row"><?php esc_html_e( 'Privacy and GDPR compliance', 'relevanssi' ); ?></th>
 		<td>
-			<?php // Translators: %1$s opens the link, %2$s closes the link. ?>
+			<?php // Translators: %1$s and %3$s open the links, %2$s closes them. ?>
 			<p><?php printf( esc_html__( '%1$sGDPR Compliance at Relevanssi knowledge base%2$s explains how using Relevanssi affects the GDPR compliance and the privacy policies of your site. Relevanssi also supports the %3$sprivacy policy tool%2$s and the WordPress user data export and erase tools.', 'relevanssi' ), "<a href='https://www.relevanssi.com/knowledge-base/gdpr-compliance/'>", '</a>', "<a href='privacy.php'>" ); ?></p>
 		</td>
 	</tr>
@@ -98,7 +105,7 @@ function relevanssi_overview_tab() {
 		<td>
 			<p><a href="https://www.relevanssi.com/buy-premium"><?php esc_html_e( 'Buy Relevanssi Premium now', 'relevanssi' ); ?></a> –
 			<?php // Translators: %1$s is the coupon code, %2$s is the year it expires. ?>
-			<?php printf( esc_html__( 'use coupon code %1$s for 20%% discount (valid at least until the end of %2$s)', 'relevanssi' ), '<strong>FREE2021</strong>', '2021' ); ?></p>
+			<?php printf( esc_html__( 'use coupon code %1$s for 20%% discount (valid at least until the end of %2$s)', 'relevanssi' ), '<strong>FREE2022</strong>', '2022' ); ?></p>
 			<p><?php esc_html_e( 'Here are some improvements Relevanssi Premium offers:', 'relevanssi' ); ?></p>
 			<ul class="relevanssi_ul">
 				<li><?php esc_html_e( 'PDF content indexing', 'relevanssi' ); ?></li>
