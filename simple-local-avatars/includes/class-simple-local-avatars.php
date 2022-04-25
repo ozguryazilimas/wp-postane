c022079c50f184c1628662289598eff76ca66f57
Onur Küçük <onur@delipenguen.net>
2022-04-25 21:36:44 +0300

    refs #60265 backport simple-local-avatars 22dk patch

--- simple-local-avatars/includes/class-simple-local-avatars.php
+++ simple-local-avatars/includes/class-simple-local-avatars.php
@@ -277,6 +277,8 @@ class Simple_Local_Avatars {
 	 * @param int   $size        Requested avatar size.
 	 */
 	public function get_simple_local_avatar_url( $id_or_email, $size ) {
+		global $wpdb;
+
 		if ( is_numeric( $id_or_email ) ) {
 			$user_id = (int) $id_or_email;
 		} elseif ( is_object( $id_or_email ) && ! empty( $id_or_email->user_id ) ) {
@@ -286,6 +288,8 @@ class Simple_Local_Avatars {
 		} elseif ( is_string( $id_or_email ) ) {
 			$user    = get_user_by( 'email', $id_or_email );
 			$user_id = $user ? $user->ID : '';
+		} else {
+			$user_id = $wpdb->get_var("SELECT user_id FROM wp_comments WHERE comment_author_email = '" . $id_or_email . "' LIMIT 1");
 		}
 
 		if ( empty( $user_id ) ) {
